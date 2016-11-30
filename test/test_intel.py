f2904ce648e450a5e24e2f74557d17640920518a
Tolja Zubow <zubow@tkn.tu-berlin.de>
2016-11-30 14:52:43 +0100

    working on csi

---
+++ test/test_intel.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import os
+import time
+import uniflex_module_wifi_intel
+
+'''
+    Direct module test; without framework.
+    Req.: Intel WiFi
+'''
+if __name__ == '__main__':
+
+    wifi = uniflex_module_wifi_intel.Iwl5300Module()
+
+    wifi.my_start_function()
+
+    csi = wifi.get_csi(3)
+
+    print(str(csi))
+
+    time.sleep(2)
+
+    wifi.my_stop_function()
